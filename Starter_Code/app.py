232c0c1789320e6165ef61934ae6d0a16592ee8a
EbukaNdu <nduaguba.victor22@gmail.com>
2024-05-08 22:44:31 -0400

    d

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,33 +1,3 @@
-# Import the dependencies.
-
-
-
-#################################################
-# Database Setup
-#################################################
-
-
-# reflect an existing database into a new model
-
-# reflect the tables
-
-
-# Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
-
-#################################################
-# Flask Setup
-#################################################
-
-
-
-
-#################################################
-# Flask Routes
-#################################################
 # Import the dependencies
 
 
